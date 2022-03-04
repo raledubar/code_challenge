ae03a34bda691a16e37c41db0ba0e8abd030ccc3
raledubar <raledubar@gmail.com>
2022-03-04 03:16:39 -0600

    MIgrations and their changes

--- trading_proj/stocks/models.py
+++ trading_proj/stocks/models.py
@@ -4,9 +4,12 @@ from django.db import models
 class Toy(models.Model):
     value = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "{}".format(self.value)
+
 
 class Stock(models.Model):
     value = models.IntegerField()
 
     def __str__(self):
-        return super().__str__()
+        return "{}".format(self.value)
