c9896e59114a66986afe5f9bf4c82182cc1d564c
raledubar <raledubar@gmail.com>
2022-03-04 03:16:11 -0600

    Creating first view and consuming api

--- trading_proj/stocks/views.py
+++ trading_proj/stocks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
 # Create your views here.
+def home_view(request):
+    url = "https://yfapi.net/v6/finance/quote"
+    querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
+    headers = {
+    'x-api-key': "YOUR-PERSONAL-API-KEY"
+    }
+    data = requests.get(url).json()
+    # data = requests.request("GET", url, headers=headers, params=querystring)
+    # print(data.text)
+    return HttpResponse(data)
