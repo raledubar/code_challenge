064a7bd5b0ac0d3dcedecbb8ae18117991e4dab6
raledubar <raledubar@gmail.com>
2022-03-04 02:53:35 -0600

    Inserting possible models

--- trading_proj/stocks/models.py
+++ trading_proj/stocks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Toy(models.Model):
+    value = models.CharField(max_length=200)
+
+
+class Stock(models.Model):
+    value = models.IntegerField()
+
+    def __str__(self):
+        return super().__str__()
