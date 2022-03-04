40ee523ecc027b244b92af7d9f99e1a99a74c109
raledubar <raledubar@gmail.com>
2022-03-04 04:10:11 -0600

    Adding environment variable

--- trading_proj/stocks/views.py
+++ trading_proj/stocks/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import os
 
 # Create your views here.
 def home_view(request):
+    var_key = "PERSONAL_API_KEY"
     url = "https://yfapi.net/v6/finance/quote"
     querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
+    #take key from an environment variable
+    personal_api = os.environ.get(var_key)
     headers = {
-    'x-api-key': "YOUR-PERSONAL-API-KEY"
+    'x-api-key': personal_api
     }
-    data = requests.get(url).json()
-    # data = requests.request("GET", url, headers=headers, params=querystring)
+    # data = requests.get(url).json()
+    data = requests.request("GET", url, headers=headers, params=querystring)
     # print(data.text)
     return HttpResponse(data)
